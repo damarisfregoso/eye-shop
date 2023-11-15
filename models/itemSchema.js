92385af717872a16d955b28832fd3b31f7d6a172
damarisfreg <fregosodmar24@gmail.com>
2023-11-15 10:41:11 -0800

    models set up

---
+++ models/itemSchema.js
@@ -0,0 +1,13 @@
+const Schema = require('mongoose').Schema;
+
+const itemSchema = new Schema({
+  name: { type: String, required: true },
+  image: String,
+  category: {type: Schema.Types.ObjectId, ref: 'Category'},
+  price: { type: Number, required: true },
+  info: { type: String, required: true },
+}, {
+  timestamps: true
+});
+
+module.exports = itemSchema;
