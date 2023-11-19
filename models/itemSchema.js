3ea4e1cbca16cb1f5480b5a55f3b7dd6b0c79614
damarisfreg <fregosodmar24@gmail.com>
2023-11-18 23:45:33 -0800

    Seeding in more products

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -2,7 +2,8 @@ const Schema = require('mongoose').Schema;
 
 const itemSchema = new Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true },
+  by: { type: String, required: true },
+  images: [{ type: String, required: true }],
   category: {type: Schema.Types.ObjectId, ref: 'Category'},
   price: { type: Number, required: true },
   info: { type: String, required: true },
--- seed.js
+++ seed.js
@@ -9,18 +9,202 @@ const Item = require('./models/item');
   const categories = await Category.create([
     {name: 'Eyes', sortOrder: 10, subcategories: ['Mascaras', 'Eyeshadows', 'Show All']},
     {name: 'Lips', sortOrder: 20, subcategories: ['Stains', 'Glosses', 'Show All']},
-    {name: 'Blushes', sortOrder: 30, subcategories: ['Liquid', 'Powder', 'Putty', 'All']},
-    {name: 'Highlighters', sortOrder: 30, subcategories: ['Liquid', 'Powder', 'Show All']},
-    {name: 'Brushes', sortOrder: 40 },
+    {name: 'Blushes', sortOrder: 30, subcategories: ['Liquid', 'Powder', 'Stick', 'Putty', 'All']},
+    {name: 'Highlighters', sortOrder: 40, subcategories: ['Liquid', 'Powder', 'Show All']},
+    {name: 'Brushes', sortOrder: 50 },
 
   ]);
 
   await Item.deleteMany({});
   const items = await Item.create([
-    {name: 'Tartelette Tubing Mascara', image: 'https://tartecosmetics.com/dw/image/v2/BJRL_PRD/on/demandware.static/-/Sites-master-catalog-tarte/default/dw78a87099/2562/RECTANGLE_MAIN/2520-tartelette-tubing-mascara-MAIN2-0.jpg?sw=2000', category: categories[10], price: 25.00, info: 'lash extensions in a tube'},
-    {name: 'Perfect Strokes Universal Volumizing Mascara', image: 'https://www.sephora.com/productimages/sku/s2474138-main-zoom.jpg?imwidth=315', category: categories[10], price: 20.00, info: 'Created for every lash type, this all-in-one mascara pairs a weightless, easy-to-build formula with a universal eye-hugging brush to lift, lengthen, curl, and volumize every lash from every angle.'},
-    {name: 'Rare Beauty Discovery Eyeshadow Palette', image: 'https://www.rarebeauty.com/cdn/shop/products/Came-to-Play-SKU.jpg?v=1634319954', category: categories[10], price: 29.00, info: 'A sleek eyeshadow palette of seven easy-to-wear, crease-resistant shades in a range of finishes that stay put all day without fading.'},
-    {name: 'Bite-Size Eyeshadow', image: 'https://www.elfcosmetics.com/dw/image/v2/BBXC_PRD/on/demandware.static/-/Sites-elf-master/default/dw7a0e260f/2020/29922_OpenA_R.jpg?sfrm=png&sw=425&q=90', category: categories[10], price: 3.00, info: 'Pigmented eyeshadow palettes that deliver 4 creamy, blendable colors in mini compacts.'},
+    {
+      name: 'Tubing Mascara', 
+      by: 'Tartelette',
+      images: [
+        'https://tartecosmetics.com/dw/image/v2/BJRL_PRD/on/demandware.static/-/Sites-master-catalog-tarte/default/dw78a87099/2562/RECTANGLE_MAIN/2520-tartelette-tubing-mascara-MAIN2-0.jpg?sw=2000'
+      ], 
+      category: categories[10], 
+      subcategories:'Mascaras', 
+      price: 25.00, 
+      info: 'lash extensions in a tube'
+    },
+    {
+      name: 'Perfect Strokes Universal Volumizing Mascara', 
+      by: 'Rare Beauty',
+      images: [
+        'https://www.sephora.com/productimages/sku/s2474138-main-zoom.jpg?imwidth=315'
+      ], 
+      category: categories[10], 
+      subcategories:'Mascaras', 
+      price: 20.00, 
+      info: 'Created for every lash type, this all-in-one mascara pairs a weightless, easy-to-build formula with a universal eye-hugging brush to lift, lengthen, curl, and volumize every lash from every angle.'
+    },
+    {
+      name: 'Discovery Eyeshadow Palette', 
+      by: 'Rare Beauty',
+      images: [
+        'https://www.rarebeauty.com/cdn/shop/products/Came-to-Play-SKU.jpg?v=1634319954'
+      ], 
+      category: categories[10], 
+      subcategories:'Eyeshadows', 
+      price: 29.00, 
+      info: 'A sleek eyeshadow palette of seven easy-to-wear, crease-resistant shades in a range of finishes that stay put all day without fading.'
+    },
+    {
+      name: 'Bite-Size Eyeshadow',
+      by: 'Elf', 
+      images: [
+        'https://www.elfcosmetics.com/dw/image/v2/BBXC_PRD/on/demandware.static/-/Sites-elf-master/default/dw7a0e260f/2020/29922_OpenA_R.jpg?sfrm=png&sw=425&q=90'
+      ], 
+      category: categories[10], 
+      subcategories:'Eyeshadows', 
+      price: 3.00, 
+      info: 'Pigmented eyeshadow palettes that deliver 4 creamy, blendable colors in mini compacts.'
+    },
+    {
+      name: 'Soft Pinch Tinted Lip Oil', 
+      by: 'Rare Beauty',
+      images: [
+        'https://www.rarebeauty.com/cdn/shop/products/soft-pinch-tinted-lip-oil-happy-1440x1952.jpg?v=1679094375'
+      ], 
+      category: categories[20], 
+      subcategories: 'Stains',
+      price: 20.00, 
+      info: 'An innovative lip jelly that transforms into a lightweight oil, starting off glossy, then leaving lips tinted while staying comfortable—never sticky—all day.'
+    },
+    {
+      name: 'Glossy Lip Stain', 
+      by: 'Elf',
+      images: [
+        'https://www.elfcosmetics.com/dw/image/v2/BBXC_PRD/on/demandware.static/-/Sites-elf-master/default/dw4d62e127/2021/81683_OpenA_R_Final.jpg?sfrm=png&sw=425&q=90'
+      ], 
+      category: categories[20], 
+      subcategories: 'Stains',
+      price: 8.00, 
+      info: 'A long lasting lip stain that gives lips a sheer pop of color and subtle gloss effect.'
+    },
+    {
+      name: 'Fat Oil Lip Drip', 
+      by: 'Nyx',
+      images: [
+        'https://m.media-amazon.com/images/I/71xY740LwFL.jpg'
+      ], 
+      category: categories[20], 
+      subcategories: 'Glosses',
+      price: 8.00, 
+      info: 'Hydrating tinted lip oil gloss'
+    },
+    {
+      name: 'Addict Lip GLow', 
+      by: 'Dior',
+      images: [
+        'https://www.dior.com/dw/image/v2/BGXS_PRD/on/demandware.static/-/Sites-master_dior/default/dw90803d5c/Y0124000/Y0124000_C012400007_E01_ZHC.jpg?sw=1800&sh=1200'
+      ], 
+      category: categories[20], 
+      subcategories: 'Glosses',
+      price: 8.00, 
+      info: 'Nourishing Lip Oil - Intense Gloss - Color-Awakening'
+    },
+    {
+      name: 'Blush Hour', 
+      by: 'Profusion',
+      images: [
+        'https://profusioncosmetics.com/cdn/shop/products/ypTXdyP8_600x600.png?v=1651264306',
+        'https://profusioncosmetics.com/cdn/shop/products/EDHASNLE_600x600.png?v=1651264306'
+      ], 
+      category: categories[30], 
+      subcategories: 'Liquid',
+      price: 6.00, 
+      info: 'This long wearing formula is a lightweight, creamy texture that seamlessly blends to a soft and matte natural finish.'
+    },
+    {
+      name: 'Soft Pinch Liquid Blush', 
+      by: 'Rare Beauty',
+      images: [
+        'https://cdn.shopify.com/s/files/1/0314/1143/7703/products/Liquid-Blush-Dewy-LUCKY-SKU.jpg?v=1645133400',
+      ], 
+      category: categories[30], 
+      subcategories: 'Liquid',
+      price: 23.00, 
+      info: 'A weightless, long-lasting liquid blush that blends and builds beautifully for a soft, healthy flush.'
+    },
+    {
+      name: 'Orgasm X Blush', 
+      by: 'Nars',
+      images: [
+        'https://www.narscosmetics.eu/dw/image/v2/BCMQ_PRD/on/demandware.static/-/Sites-itemmaster_NARS/default/dwc4dc8436/hi-res/MINI-BLUSH/OrgasmX_Mini_BlushA.jpg?sw=856&sh=750&sm=fit',
+      ], 
+      category: categories[30], 
+      subcategories: 'Powder',
+      price: 17.00, 
+      info: 'A travel-size of the bestselling, iconic powder blush.'
+    },
+    {
+      name: 'Soft Matte Perfection Blush Duos',
+      by: 'Sephora', 
+      images: [
+        'https://www.sephora.com/productimages/sku/s2055861-main-zoom.jpg?imwidth=630',
+      ], 
+      category: categories[30], 
+      subcategories: 'Powder',
+      price: 8.00, 
+      info: 'A modern powder blush with a creamy, buildable texture and a soft matte finish.'
+    },
+    {
+      name: 'Sweet Blush Heart Shaped Sitck Blush', 
+      by: 'GlamourUs',
+      images: [
+        'https://glamourusus.com/cdn/shop/products/glamour-us-amorus-sweet-blush-heart-shaped-stick-blush-makeup-sweet-co-hbl-01-926319.jpg?v=1692249877&width=1100',
+      ], 
+      category: categories[30], 
+      subcategories: 'Stick',
+      price: 5.99, 
+      info: 'The Sweet Blush Heart Shaped Stick Blush is designed to let you enjoy this creamy and ready-to-roll formula at the tip of your hands.'
+    },
+    {
+      name: 'Cheeky Tint Cream Blush Stick', 
+      by: 'Huda Beauty',
+      images: [
+        'https://www.sephora.com/productimages/sku/s2555779-main-zoom.jpg?imwidth=630'
+      ], 
+      category: categories[30], 
+      subcategories: 'Stick',
+      price: 25.00, 
+      info: 'A beautifully buildable cream blush that melts into skin, gliding seamlessly over the complexion with a dewy hint of tint for major second-skin vibes and a juicy glow.'
+    },
+    {
+      name: 'Luminous Putty Blush',
+      by: 'Elf',
+      images: [
+        'https://media.ulta.com/i/ulta/2601104?w=1036&h=1036'
+      ], 
+      category: categories[30], 
+      subcategories: 'Putty',
+      price: 7.00, 
+      info: 'A putty-to-powder, buildable blush that adds a pop of color to your complexion with a subtle, shimmer finish.'
+    },
+    {
+      name: 'Positive Light Liquid Luminizer Highlight', 
+      by: 'Rare Beauty',
+      images: [
+        'https://www.sephora.com/productimages/sku/s2362168-main-zoom.jpg?imwidth=630'
+      ], 
+      category: categories[40], 
+      subcategories: 'Liquid ',
+      price: 25.00, 
+      info: 'A silky, second-skin liquid highlighter that creates a lasting, soft, and luminous finish.'
+    },
+    {
+      name: 'High Beam Satin Pink Liquid Highlighter', 
+      by: 'Benefit Cosmetics',
+      images: [
+        'https://www.sephora.com/productimages/sku/s2264646-main-zoom.jpg?imwidth=630'
+      ], 
+      category: categories[40], 
+      subcategories: 'Liquid',
+      price: 24.00, 
+      info: 'A satiny-pink liquid highlighter that accents cheek and brow bones for a dewy glow.'
+    },
 
   ]);
 
