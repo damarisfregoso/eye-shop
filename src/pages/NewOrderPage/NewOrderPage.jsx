3493f0c0ab1d8dfeaffdc452e50fac761639419f
damarisfreg <fregosodmar24@gmail.com>
2023-12-15 23:53:01 -0800

    Implementing useState and useEffect for makeup items

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,4 +1,14 @@
+import { useState, useEffect } from "react";
+
 export default function NewOrderPage() {
+
+  const [makeupItems, setMakeupItems] = useState([]);
+
+  useEffect(function() {
+    console.log('NewOrderPage rendered');
+  });
+
+
   return (
     <h1>NewOrderPage</h1>
   );
