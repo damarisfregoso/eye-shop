aa5abde3b04b4c9916a1c381376519b3c60599ee
damarisfreg <fregosodmar24@gmail.com>
2023-12-18 23:55:42 -0800

    data is now in state

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
+import * as itemsAPI from '../../utilities/items-api';
 
 export default function NewOrderPage() {
 
   const [makeupItems, setMakeupItems] = useState([]);
 
   useEffect(function() {
-
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      setMakeupItems(items);
+    }
+    getItems();
   }, []);
 
   return (
