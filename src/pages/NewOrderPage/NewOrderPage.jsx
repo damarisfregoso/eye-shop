f7ad11bd83ecb6e373ebb90db1ce0140c5bddc22
damarisfreg <fregosodmar24@gmail.com>
2023-12-19 23:49:50 -0800

    implementing useRef for categories

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,13 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import * as itemsAPI from '../../utilities/items-api';
 
 export default function NewOrderPage() {
 
   const [makeupItems, setMakeupItems] = useState([]);
+  const categoriesRef = useRef([]);
 
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
+      categoriesRef.current = [...new Set (items.map(item => item.category.name))];
       setMakeupItems(items);
     }
     getItems();
