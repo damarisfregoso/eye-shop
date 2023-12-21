c2a6c5ed5ca09ecb86e52542e689c08445aeaa2d
damarisfreg <fregosodmar24@gmail.com>
2023-12-20 23:53:40 -0800

    Cleaning up code and fixing up new order page

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect, useRef } from "react";
 import * as itemsAPI from '../../utilities/items-api';
+import './NewOrderPage.css'
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MakeupList from '../../components/MakeupList/MakeupList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
 export default function NewOrderPage() {
 
@@ -16,6 +23,21 @@ export default function NewOrderPage() {
   }, []);
 
   return (
-    <h1>NewOrderPage</h1>
+    <main className="NewOrderPage">
+  <aside>
+    <Logo />
+    <CategoryList
+      categories={categoriesRef.current}
+      activeCat={activeCat}
+      setActiveCat={setActiveCat}
+    />
+    <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+    <UserLogOut user={user} setUser={setUser} />
+  </aside>
+  <MenuList
+    menuItems={menuItems.filter(item => item.category.name === activeCat)}
+  />
+  <OrderDetail />
+</main>
   );
 }
