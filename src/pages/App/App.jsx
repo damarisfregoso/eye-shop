69420a4a30ce263764eb7f704a7c2f28dad76bd4
damarisfreg <fregosodmar24@gmail.com>
2024-01-03 23:10:56 -0800

    Changed NewOrderPage to welcome page, ccategories are currently being displayed

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,14 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 import WelcomePage from '../WelcomePage/WelcomePage';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
-  const [makeupItems, setMakeupItems] = useState([]);
   const [activeCat, setActiveCat] = useState('');
 
   return (
@@ -20,8 +18,7 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
-              <Route path="/" element={<WelcomePage user={user}/>} />
-              <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
+              <Route path="/orders/new" element={<WelcomePage user={user} setUser={setUser} />} />
               <Route path="/orders" element={<OrderHistoryPage />} />
             </Routes>
           </>
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,9 +1,31 @@
-export default function WelcomePage({user}) {
+import { useState, useEffect, useRef } from "react";
+import * as itemsAPI from '../../utilities/items-api';
+// import { Link } from 'react-router-dom';
+import CategoryList from '../../components/CategoryList/CategoryList';
+// import OrderDetail from '../../components/OrderDetail/OrderDetail';
+
+export default function WelcomePage( user, setUser, makeupItems, setMakeupItems) {
+
+  const [activeCat, setActiveCat] = useState('');
+  const categoriesRef = useRef([]);
+
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      categoriesRef.current = [...new Set (items.map(item => item.category.name))];
+      setActiveCat(categoriesRef.current[0])
+    }
+    getItems();
+  }, []);
 
   return (
-    <div>
-      <h1>Welcome {user.name}</h1>
-      <h2>Browse all your favorites now!</h2>
-    </div>
-  )
+    <main className="WelcomePage">
+      <h1>Welcome to Eye Shop, get all the goodies here.</h1>
+      <CategoryList 
+        categories={categoriesRef.current}
+        activeCat={activeCat}
+        setActiveCat={setActiveCat}
+      />
+    </main>
+  );
 }
