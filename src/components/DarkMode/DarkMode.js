700ab923a58de033301443dd0c142f34ee5a0531
damarisfreg <fregosodmar24@gmail.com>
2024-01-10 23:55:51 -0800

    Dark/Light mode setting is now working and being stored in local storage for a better user experience

--- src/components/DarkMode/DarkMode.js
+++ src/components/DarkMode/DarkMode.js
@@ -6,11 +6,18 @@ import "./DarkMode.css";
 const DarkMode = () => {
 
     const setDarkMode = () => {
-        document.querySelector("body").setAttribute('data-theme', 'dark')
+        document.querySelector('body').setAttribute('data-theme', 'dark')
+        localStorage.setItem('selectedTheme', 'dark')
     }
     const setLightMode = () => {
         document.querySelector("body").setAttribute('data-theme', 'light')
+        localStorage.setItem('selectedTheme', 'light')
     }
+
+    const selectedTheme = localStorage.getItem('selectedTheme');
+        if (selectedTheme === "dark") {
+            setDarkMode();
+        }
 
     const toggleTheme = (e) => {
         if (e.target.checked) setDarkMode();
@@ -23,6 +30,7 @@ const DarkMode = () => {
                 type='checkbox'
                 id='darkmode-toggle'
                 onChange={toggleTheme}
+                defaultChecked={selectedTheme === 'dark'}
             />
             <label className='dark_mode_label' for='darkmode-toggle'>
                 <Sun />
