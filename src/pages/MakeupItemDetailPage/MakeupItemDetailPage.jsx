05e6b7e7d152749724a71742311e6387f58b2d01
damarisfreg <fregosodmar24@gmail.com>
2024-02-08 23:56:30 -0800

    Displaying details for makeup items

--- src/pages/MakeupItemDetailPage/MakeupItemDetailPage.jsx
+++ src/pages/MakeupItemDetailPage/MakeupItemDetailPage.jsx
@@ -9,18 +9,27 @@ export default function MakeupItemDetailPage({ makeupItems }) {
 
   return (
     <div className='MakeupItemDetailPage'>
-      <h1>{item}</h1>
-      {selectedItem.images.length > 1 ? (
-        <Carousel>
-          {selectedItem.images.map((image, index) => (
-            <Carousel.Item key={index}>
-              <img className="d-block w-100" src={image} alt={`Slide ${index}`} />
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      ) : (
-        <img src={selectedItem.images[0]} alt={selectedItem.name} />
-      )}
+      <div>
+
+      
+        <h1>{item}</h1>
+        {selectedItem.images.length > 1 ? (
+          <Carousel>
+            {selectedItem.images.map((image, index) => (
+              <Carousel.Item key={index}>
+                <img className="d-block w-100" src={image} alt={`Slide ${index}`} />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        ) : (
+          <img src={selectedItem.images[0]} alt={selectedItem.name} />
+        )}
+      </div>
+      <div>
+        <h1>Details</h1>
+        <p>{selectedItem.info}</p>
+        <p>${selectedItem.price.toFixed(2)}</p>
+      </div>
     </div>
   );
 }
