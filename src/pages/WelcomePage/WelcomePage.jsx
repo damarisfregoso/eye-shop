d78f496a845b0f903f1de1fa231fca66967cb4f2
damarisfreg <fregosodmar24@gmail.com>
2024-03-27 23:37:10 -0700

    Welcome page is now displaying as links

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import CategoryList from "../../components/CategoryList/CategoryList";
 import './WelcomePage.css';
 
@@ -17,19 +18,22 @@ export default function WelcomePage({ user, setUser, categoriesRef, categoryInfo
 
       {categoriesRef.current.map(category => (
         <div key={category}>
-        <h2>{category}</h2>
-          {categoryInfo[category] && (
-            <div className="Category-Box">
-              <div className="Box-1">
-                <img src={categoryInfo[category].image} alt={`Random ${category}`} />
+          <h2>{category}</h2>        
+          <Link key={category} to={`/category/${category}`}>
+            {categoryInfo[category] && (
+              <div className="Category-Box">
+                <div className="Box-1">
+                  <img src={categoryInfo[category].image} alt={`Random ${category}`} />
+                </div>
+                <div className="Box-2">
+                  <h1>{categoryInfo[category].name}</h1>
+                  <p>{categoryInfo[category].info}</p>
+                </div>
               </div>
-              <div className="Box-2">
-                <h1>{categoryInfo[category].name}</h1>
-                <p>{categoryInfo[category].info}</p>
-              </div>
-            </div>
-          )}
+            )}
+          </Link>
         </div>
+
       ))}
     </main>
   );
